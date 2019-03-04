ed844a6a1c06333e05460dab75f37fcd8147beb1
adambeck7 <adambeck7@gmail.com>
2019-03-03 17:56:40 -0700

    adding dist folder for deploy

---
+++ dist/precache-manifest.1cb02144590046f8640d0abd6ef73f0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cd9813858b09af7222df",
+    "url": "/js/chunk-vendors.475f8607.js"
+  },
+  {
+    "revision": "c13bfacc2b11eee5ea32",
+    "url": "/js/app.90420898.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "/js/about.41d636ee.js"
+  },
+  {
+    "revision": "42684247ecb7c6c3d2bd5ec7670a50ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c13bfacc2b11eee5ea32",
+    "url": "/css/app.be59f151.css"
+  }
+];
